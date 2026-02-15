7d4784c00c05357fe44f1d1d2df1fe073d402e39
arenekosreal <17194552+arenekosreal@users.noreply.github.com>
2026-02-15 17:48:44 +0800

    Apply formatting

--- CloneDash.Sdl/RendererTextEngine.cs
+++ CloneDash.Sdl/RendererTextEngine.cs
@@ -12,10 +12,13 @@ internal readonly struct RendererTextEngine : ITextEngine
 
     public ValueTask<IText> CreateTextAsync(IFont font, string text) =>
         ValueTask.FromResult<IText>(
-            new Text() { SdlPtr = TTF.CreateText(SdlPtr,
-                                                 font.SdlPtr,
-                                                 text,
-                                                 Convert.ToUInt32(Encoding.UTF8.GetByteCount(text))) });
+            new Text()
+            {
+                SdlPtr = TTF.CreateText(SdlPtr,
+                                        font.SdlPtr,
+                                        text,
+                                        Convert.ToUInt32(Encoding.UTF8.GetByteCount(text)))
+            });
 
     public ValueTask DisposeAsync()
     {
